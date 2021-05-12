6c5ee639973fe25e5ccaf47f49a1d8cf5d54fa3e
60160113 <60160113@go.buu.ac.th>
2021-05-12 11:02:02 +0700

    hide menu

--- src/router/audit.js
+++ src/router/audit.js
@@ -2,11 +2,17 @@ export default [
   {
     path: "/files-report",
     name: "Files Report",
+    meta: {
+      restriction: "GROUP_ALFRESCO_ADMINISTRATORS"
+    },
     component: () => import("@/views/audit/Files-Report.vue")
   },
   {
     path: "/users-report",
     name: "Users Report",
+    meta: {
+      restriction: "GROUP_ALFRESCO_ADMINISTRATORS"
+    },
     component: () => import("@/views/audit/Users-Report.vue")
   }
 ];
