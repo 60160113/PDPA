31d0b477273ba88d4e7214ad58e851744685c1fe
60160113 <60160113@go.buu.ac.th>
2021-05-13 13:37:17 +0700

    Update Login.vue

--- src/views/pages/Login.vue
+++ src/views/pages/Login.vue
@@ -5,65 +5,46 @@
   >
     <CContainer>
       <CRow class="justify-content-center">
-        <CCol md="8">
-          <CCardGroup>
-            <CCard class="p-4">
-              <CCardBody>
-                <CForm>
-                  <h1>Login</h1>
-                  <p class="text-muted">Sign In to your account</p>
-                  <CInput
-                    placeholder="Username"
-                    autocomplete="username email"
-                    v-model="user.userId"
-                  >
-                    <template #prepend-content
-                      ><CIcon name="cil-user"
-                    /></template>
-                  </CInput>
-                  <CInput
-                    placeholder="Password"
-                    type="password"
-                    autocomplete="curent-password"
-                    v-model="user.password"
-                  >
-                    <template #prepend-content
-                      ><CIcon name="cil-lock-locked"
-                    /></template>
-                  </CInput>
-                  <CRow>
-                    <CCol col="6" class="text-left">
-                      <CButton color="primary" class="px-4" @click="login"
-                        >Login</CButton
-                      >
-                    </CCol>
-                    <CCol col="6" class="text-right">
-                      <CButton color="link" class="px-0"
-                        >Forgot password?</CButton
-                      >
-                      <CButton color="link" class="d-lg-none"
-                        >Register now!</CButton
-                      >
-                    </CCol>
-                  </CRow>
-                </CForm>
-              </CCardBody>
-            </CCard>
-            <CCard
-              color="primary"
-              text-color="white"
-              class="text-center py-5 d-md-down-none"
-              body-wrapper
-            >
-              <CCardBody>
-                <h2>Sign up</h2>
-                <br />
-                <CButton color="light" variant="outline" size="lg">
-                  Register Now!
-                </CButton>
-              </CCardBody>
-            </CCard>
-          </CCardGroup>
+        <CCol lg="6">
+          <CCard class="p-4">
+            <CCardBody>
+              <CForm>
+                <h1>Login</h1>
+                <p class="text-muted">Sign In to your account</p>
+                <CInput
+                  placeholder="Username"
+                  autocomplete="username email"
+                  v-model="user.userId"
+                >
+                  <template #prepend-content
+                    ><CIcon name="cil-user"
+                  /></template>
+                </CInput>
+                <CInput
+                  placeholder="Password"
+                  type="password"
+                  autocomplete="curent-password"
+                  v-model="user.password"
+                >
+                  <template #prepend-content
+                    ><CIcon name="cil-lock-locked"
+                  /></template>
+                </CInput>
+                <CRow>
+                  <CCol col="6" class="text-left">
+                    <CButton color="primary" class="px-4" @click="login"
+                      >Login</CButton
+                    >
+                  </CCol>
+                  <CCol col="6" class="text-right">
+                    <!-- <CButton color="link" class="px-0"
+                      >Forgot password?</CButton
+                    > -->
+                  </CCol>
+                </CRow>
+              </CForm>
+            </CCardBody>
+          </CCard>
         </CCol>
       </CRow>
     </CContainer>
