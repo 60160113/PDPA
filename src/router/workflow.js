607a8f236c78e5806a0073f88df36c1a81f63eaa
Tantai Thanakanok <leknarm@gmail.com>
2021-06-21 20:35:48 +0700

    get latest token and redirect

--- src/router/workflow.js
+++ src/router/workflow.js
@@ -1,11 +1,10 @@
-const user = JSON.parse(localStorage.getItem("user"));
-
 export default [
   {
     path: "/workflow",
     name: "Workflow",
     beforeEnter(to, from, next) {
-      const url = `${process.env.VUE_APP_WF_SSO_URL}?${process.env.VUE_APP_WF_SSO_PARAMETER}=${user.token}`
+      const user = JSON.parse(localStorage.getItem("user"));
+      const url = `${process.env.VUE_APP_WF_SSO_URL}?${process.env.VUE_APP_WF_SSO_PARAMETER}=${user.token}`;
       window.open(url, '_blank');
     }
   }
