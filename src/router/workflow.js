2141ba8bf763eb2e5aa2f2ff05526cd3734b8565
Tantai Thanakanok <leknarm@gmail.com>
2021-06-24 12:55:10 +0700

    add redirect url when token expire

--- src/router/workflow.js
+++ src/router/workflow.js
@@ -4,7 +4,8 @@ export default [
     name: "Workflow",
     beforeEnter(to, from, next) {
       const user = JSON.parse(localStorage.getItem("user"));
-      const url = `${process.env.VUE_APP_WF_SSO_URL}?${process.env.VUE_APP_WF_SSO_PARAMETER}=${user.token}`;
+      const host = window.location.protocol.concat('//').concat(window.location.host);
+      const url = `${process.env.VUE_APP_WF_SSO_URL}?${process.env.VUE_APP_WF_SSO_PARAMETER}=${user.token}&expire_url=${encodeURIComponent(host)}`;
       window.open(url, '_blank');
     }
   }
