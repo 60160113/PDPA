9eb30119f72cc600a9e07b7693249649069a62a2
60160113 <60160113@go.buu.ac.th>
2021-07-08 14:09:58 +0700

    Update TheFooter.vue

--- src/containers/TheFooter.vue
+++ src/containers/TheFooter.vue
@@ -2,7 +2,8 @@
   <CFooter :fixed="false">
     <div>
       <span class="ml-1">
-        &copy; {{ new Date().getFullYear() }} IV Soft Co., Ltd.
+        &copy; {{ new Date().getFullYear() }}
+        <a href="http://www.ivsoft.co.th/" target="_blank">IV Soft Co., Ltd.</a>
       </span>
     </div>
   </CFooter>
