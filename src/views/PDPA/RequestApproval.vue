10796f3662925e01c956fe6f70b8e00077079eda
60160113 <60160113@go.buu.ac.th>
2021-07-13 16:14:28 +0700

    Update RequestApproval.vue

--- src/views/PDPA/RequestApproval.vue
+++ src/views/PDPA/RequestApproval.vue
@@ -9,10 +9,11 @@
         <CDataTable
           :items="requests"
           :fields="[
-            { key: 'name', label: 'Name', _style: 'width:30%' },
+            { key: 'name', label: 'Name', _style: 'width:25%' },
             { key: 'requester', label: 'Requester', _style: 'width:20%' },
             { key: 'consents', label: 'Consents', _style: 'width:20%' },
             { key: 'createdAt', label: 'Created At', _style: 'width:15%' },
+            { key: 'status', label: 'Status', _style: 'width:5%' },
             { key: 'actions', label: 'Actions', _style: 'width:15%' },
           ]"
           :tableFilter="{
@@ -24,6 +25,7 @@
           :itemsPerPageSelect="{
             label: 'แสดง',
           }"
+          :loading="loading"
           hover
           striped
           border
@@ -34,6 +36,15 @@
           <template #name="{ item }">
             <td>
               <p class="text-primary">{{ item.data.name }}</p>
+              <span v-if="item.publish.isPublished">
+                Link:
+                <CLink
+                  :href="`http://localhost:8080/#/property/${item.publish.id}`"
+                  target="_blank"
+                >
+                  http://localhost:8080/#/property/{{ item.publish.id }}
+                </CLink>
+              </span>
             </td>
           </template>
 
@@ -50,9 +61,26 @@
             </td>
           </template>
 
+          <template #status="{ item }">
+            <td>
+              <CBadge
+                style="font-size: 15px"
+                :color="item.publish.isPublished ? 'success' : 'primary'"
+              >
+                {{ item.publish.isPublished ? "อนุมัติ" : "รออนุมัติ" }}
+              </CBadge>
+            </td>
+          </template>
+
           <template #actions="{ item }">
             <td>
-              <CButton color="success" @click="approveRequest(item)"> อนุมัติ </CButton>&nbsp;
+              <CButton
+                color="success"
+                :disabled="item.publish.isPublished"
+                @click="approveRequest(item)"
+              >
+                อนุมัติ </CButton
+              >&nbsp;
               <CButton color="danger" @click="removeRequest(item['_id'])">
                 ไม่อนุมัติ
               </CButton>
@@ -72,14 +100,18 @@ export default {
   data() {
     return {
       requests: [],
+
+      loading: false,
     };
   },
   methods: {
     getRequests() {
+      this.loading = true;
       this.$http
         .get(`${process.env.VUE_APP_PDPA_SERVICES}personal_data`)
         .then((res) => {
           this.requests = res.data;
+          this.loading = false;
         });
     },
     removeRequest(id) {
@@ -90,13 +122,18 @@ export default {
         });
     },
     approveRequest(item) {
+      this.loading = true;
       this.$http
-        .post(`${process.env.VUE_APP_PDPA_SERVICES}publish_data`, {
-          data: item.data,
-          requester: item.requester,
-        })
-        .then((res) => {
-          console.log(res.data);
+        .post(
+          `${process.env.VUE_APP_PDPA_SERVICES}publish_data/${item["_id"]}`,
+          {
+            data: item.data,
+            requester: item.requester,
+          }
+        )
+        .then(() => {
+          this.getRequests();
+          this.loading = false;
         });
     },
   },
