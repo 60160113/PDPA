df9c80cfde6e40fc70b7d6aa912de4612134a326
60160113 <60160113@go.buu.ac.th>
2021-07-14 13:36:36 +0700

    Update menu.js

--- src/containers/menu.js
+++ src/containers/menu.js
@@ -1,71 +1,71 @@
 export default [
-  // Files
-  {
-    _name: "CSidebarNavTitle",
-    _children: ["Files"]
-  },
-  {
-    _name: "CSidebarNavItem",
-    name: "My Files",
-    to: "/myfiles",
-    icon: "cil-file"
-  },
-  {
-    _name: "CSidebarNavItem",
-    name: "Shared Files",
-    to: "/sharedfiles",
-    icon: "cil-file"
-  },
-  {
-    _name: "CSidebarNavItem",
-    name: "Repository",
-    to: "/repository",
-    icon: "cil-file"
-  },
-  // Search
-  {
-    _name: "CSidebarNavTitle",
-    _children: ["Search"]
-  },
-  {
-    _name: "CSidebarNavItem",
-    name: "Search",
-    to: "/search",
-    icon: "cil-search"
-  },
-  // Audit
-  {
-    _name: "CSidebarNavTitle",
-    _children: ["Audit"],
-    restriction: "GROUP_ALFRESCO_ADMINISTRATORS"
-  },
-  {
-    _name: "CSidebarNavItem",
-    name: "Files Report",
-    to: "/files-report",
-    icon: "cil-history"
-  },
-  {
-    _name: "CSidebarNavItem",
-    name: "Users Report",
-    to: "/users-report",
-    icon: "cil-people"
-  },
-  // PDPA
-  {
-    _name: "CSidebarNavTitle",
-    _children: ["PDPA"]
-  },
-  {
-    _name: "CSidebarNavItem",
-    name: "Request Data",
-    to: "/request",
-    icon: "cil-people"
-  },
-  {
-    _name: "CSidebarNavItem",
-    name: "Request Approval",
-    to: "/request-approval",
-    icon: "cil-people"
-  },
+    // Files
+    {
+        _name: "CSidebarNavTitle",
+        _children: ["Files"]
+    },
+    {
+        _name: "CSidebarNavItem",
+        name: "My Files",
+        to: "/myfiles",
+        icon: "cil-file"
+    },
+    {
+        _name: "CSidebarNavItem",
+        name: "Shared Files",
+        to: "/sharedfiles",
+        icon: "cil-file"
+    },
+    {
+        _name: "CSidebarNavItem",
+        name: "Repository",
+        to: "/repository",
+        icon: "cil-file"
+    },
+    // Search
+    {
+        _name: "CSidebarNavTitle",
+        _children: ["Search"]
+    },
+    {
+        _name: "CSidebarNavItem",
+        name: "Search",
+        to: "/search",
+        icon: "cil-search"
+    },
+    // Audit
+    {
+        _name: "CSidebarNavTitle",
+        _children: ["Audit"],
+        restriction: "GROUP_ALFRESCO_ADMINISTRATORS"
+    },
+    {
+        _name: "CSidebarNavItem",
+        name: "Files Report",
+        to: "/files-report",
+        icon: "cil-history"
+    },
+    {
+        _name: "CSidebarNavItem",
+        name: "Users Report",
+        to: "/users-report",
+        icon: "cil-people"
+    },
+    // PDPA
+    {
+        _name: "CSidebarNavTitle",
+        _children: ["PDPA"]
+    },
+    {
+        _name: "CSidebarNavItem",
+        name: "Request Data",
+        to: "/request",
+        icon: "cil-lock-locked"
+    },
+    {
+        _name: "CSidebarNavItem",
+        name: "Request Approval",
+        to: "/request-approval",
+        icon: "cil-check"
+    },
 ];
