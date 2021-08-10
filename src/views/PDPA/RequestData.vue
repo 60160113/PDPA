6748f7415dc3c31137ce0def4779995e79fcf5bf
60160113 <60160113@go.buu.ac.th>
2021-08-10 13:08:34 +0700

    t

--- src/views/PDPA/RequestData.vue
+++ src/views/PDPA/RequestData.vue
@@ -10,14 +10,14 @@
         <CRow class="mt-3">
           <CCol col="2">
             <label style="margin-top: 5px">
-              เจ้าของข้อมูล&nbsp;
+              ขอเอกสาร&nbsp;
               <b style="color: red">*</b>
             </label>
           </CCol>
           <CCol>
             <v-select
-              v-model="personalDataId"
-              :options="personalDataOptions"
+              v-model="dataType"
+              :options="dataTypeOptions"
               :reduce="(item) => item.id"
               label="name"
               placeholder="กรุณาเลือก"
@@ -25,85 +25,53 @@
           </CCol>
         </CRow>
 
-        <CRow class="mt-3" v-if="personalDataId">
+        <CInputRadioGroup
+          class="mt-3"
+          :options="[
+            { value: 'individual', label: 'รายบุคคล' },
+            {
+              value: 'department',
+              label: 'แผนก',
+            },
+          ]"
+          custom
+          inline
+        />
+
+        <CRow class="mt-3" v-if="dataType">
           <CCol col="2">
             <label style="margin-top: 5px">
-              ข้อมูล&nbsp;
+              ชื่อพนักงาน&nbsp;
               <b style="color: red">*</b>
             </label>
           </CCol>
           <CCol>
-            <v-select
-              v-model="data"
-              :options="dataOptions"
-              label="name"
-              placeholder="กรุณาเลือก"
+            <CMultiSelect
+              v-if="documentOptions.length != 0"
+              :options="documentOptions"
+              :search="true"
+              inline
+              @update="
+                (value) => {
+                  personalData.documents = value;
+                }
+              "
             />
           </CCol>
         </CRow>
 
-        <hr />
-
-        <CRow>
-          <CCol>
-            <b style="font-size: 16px" class="text-primary">Consents</b>
-          </CCol>
-          <CCol class="d-flex justify-content-end">
-            <CButton
-              color="primary"
-              v-c-tooltip="'เพิ่ม'"
-              @click="consents.push('')"
-            >
-              <CIcon name="cil-plus" />&nbsp;เพิ่ม
-            </CButton>
-          </CCol>
-        </CRow>
-        <br />
-        <CRow v-for="(item, index) in consents" :key="index">
-          <CCol md="10" sm="6">
-            <CTextarea
-              :label="`Consent ที่ ${index + 1}:`"
-              horizontal
-              v-model="consents[index]"
-            />
-          </CCol>
-          <CCol class="text-right">
-            <CButton
-              color="danger"
-              v-c-tooltip="'ลบ'"
-              @click="consents.splice(index, 1)"
-            >
-              <CIcon name="cil-trash" />
-            </CButton>
-          </CCol>
-        </CRow>
-
-        <div class="text-right">
-          <CButton
-            color="primary"
-            :disabled="!personalDataId || !data"
-            @click="request"
-          >
-            บันทึก
-          </CButton>
+        <div class="mt-3">
+          <CInputCheckbox
+            class="mb-2"
+            :key="index"
+            v-for="(item, index) in documentTypeOptions"
+            custom
+            :label="item.label"
+            :value="item.value"
+          />
         </div>
       </CCardBody>
     </CCard>
-
-    <!-- notify -->
-    <CModal
-      :show.sync="notify"
-      :no-close-on-backdrop="true"
-      :centered="true"
-      title="เสร็จสิ้น"
-      size="sm"
-      color="success"
-    >
-      ส่งคำร้องขอเสร็จสิ้น
-      <template #footer>
-        <CButton @click="notify = false" color="secondary"> ปิด </CButton>
-      </template>
-    </CModal>
 
     <CElementCover :opacity="0.7" v-show="loading" />
   </div>
@@ -118,50 +86,52 @@ export default {
     vSelect,
   },
   async created() {
-    this.personalDataOptions = await this.getNodeChildrenById(
+    this.dataTypeOptions = await this.getNodeChildrenById(
       process.env.VUE_APP_PERSONAL_DATA_FOLDER,
-      { where: "(isFolder=true)", fields: ["name", "id"] }
+      { where: "(isFolder=true)", fields: "name,id" }
     );
+    this.$http
+      .get(`${process.env.VUE_APP_PDPA_SERVICES}data/document_type`)
+      .then((res) => {
+        this.documentTypeOptions = res.data;
+      });
   },
   watch: {
-    personalDataId: async function (id) {
+    dataType: async function (id) {
       if (id) {
-        this.dataOptions = await this.getNodeChildrenById(id, {
-          where: "(isFile=true)",
-          fields: ["name", "id"],
+        this.documentOptions = await this.getNodeChildrenById(id, {
+          where: "(isFolder=true)",
+          fields: "name,id",
         });
-
-        const filter = await this.$http.get(
-          `${process.env.VUE_APP_PDPA_SERVICES}personal_data?status=pending&status=approved&requester.id=${this.$store.state.user.userId}`
-        );
-
-        this.dataOptions = await this.dataOptions.filter((element) => {
-          return (
-            filter.data.filter((item) => item.data.id == element.id).length == 0
-          );
+        this.documentOptions = this.documentOptions.map((item) => {
+          return {
+            value: item,
+            text: item.name,
+          };
         });
-
-        this.data = this.dataOptions[0];
       } else {
-        this.dataOptions = [];
-        this.data = null;
+        this.documentOptions = [];
       }
     },
   },
   data() {
     return {
-      personalDataOptions: [],
-      personalDataId: null,
+      personalData: {
+        requester: {
+          id: this.$store.state.user.userId,
+          name: this.$store.state.user.displayName,
+        },
+        documents: [],
+      },
 
-      dataOptions: [],
+      dataType: null,
+      dataTypeOptions: [],
 
-      data: null,
+      documentOptions: [],
 
-      consents: [""],
+      documentTypeOptions: [],
 
       loading: false,
-
-      notify: false,
     };
   },
   methods: {
@@ -172,18 +142,16 @@ export default {
         let skipCount = 0;
         let arr = [];
 
-        const BASE_URL = `${process.env.VUE_APP_ALFRESCO_API}alfresco/versions/1/nodes/${id}/children`;
-
-        let query_string = "";
-
-        await Object.keys(query).forEach((key) => {
-          query_string += `&${key}=${query[key]}`;
-        });
-
         do {
-          const res = await this.$http.get(
-            `${BASE_URL}?maxItems=${maxItems}&skipCount=${skipCount}${query_string}`
-          );
+          const res = await this.$http({
+            method: "get",
+            url: `${process.env.VUE_APP_ALFRESCO_API}alfresco/versions/1/nodes/${id}/children`,
+            params: {
+              maxItems,
+              skipCount,
+              ...query,
+            },
+          });
 
           arr.push(...res.data.list.entries.map((item) => item.entry));
 
@@ -195,30 +163,6 @@ export default {
       } catch (error) {
         return [];
       }
-    },
-    request() {
-      this.loading = true;
-      this.$http
-        .post(`${process.env.VUE_APP_PDPA_SERVICES}personal_data`, {
-          requester: {
-            id: this.$store.state.user.userId,
-            name: this.$store.state.user.displayName,
-          },
-          data: this.data,
-
-          consents: this.consents,
-        })
-        .then(() => {
-          setTimeout(() => {
-            this.loading = false;
-            this.personalDataId = null;
-
-            this.notify = true;
-          }, 1000);
-        })
-        .catch((err) => {
-          this.loading = false;
-        });
     },
   },
 };
