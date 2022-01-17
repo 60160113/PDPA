6301f3fcc366918b16f57cffb9b4b948c2f0af1e
60160113 <60160113@go.buu.ac.th>
2022-01-17 15:00:43 +0700

    Update Register.vue

--- src/views/pages/Register.vue
+++ src/views/pages/Register.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="c-app d-flex align-items-center min-vh-100" :class="{ 'c-dark-theme': $store.state.darkMode }">
+  <div
+    class="c-app d-flex align-items-center min-vh-100"
+    :class="{ 'c-dark-theme': $store.state.darkMode }"
+  >
     <CContainer fluid>
       <CRow class="justify-content-center">
         <CCol md="6">
@@ -8,23 +11,20 @@
               <CForm>
                 <h1>Register</h1>
                 <p class="text-muted">Create your account</p>
-                <CInput
-                  placeholder="Username"
-                  autocomplete="username"
-                >
-                  <template #prepend-content><CIcon name="cil-user"/></template>
+                <CInput placeholder="Username" autocomplete="username">
+                  <template #prepend-content
+                    ><CIcon name="cil-user"
+                  /></template>
                 </CInput>
-                <CInput
-                  placeholder="Email"
-                  autocomplete="email"
-                  prepend="@"
-                />
+                <CInput placeholder="Email" autocomplete="email" prepend="@" />
                 <CInput
                   placeholder="Password"
                   type="password"
                   autocomplete="new-password"
                 >
-                  <template #prepend-content><CIcon name="cil-lock-locked"/></template>
+                  <template #prepend-content
+                    ><CIcon name="cil-lock-locked"
+                  /></template>
                 </CInput>
                 <CInput
                   placeholder="Repeat password"
@@ -32,24 +32,14 @@
                   autocomplete="new-password"
                   class="mb-4"
                 >
-                  <template #prepend-content><CIcon name="cil-lock-locked"/></template>
+                  <template #prepend-content
+                    ><CIcon name="cil-lock-locked"
+                  /></template>
                 </CInput>
-                <CButton color="success" block>Create Account</CButton>
               </CForm>
             </CCardBody>
             <CCardFooter class="p-4">
-              <CRow>
-                <CCol col="6">
-                  <CButton block color="facebook">
-                    Facebook
-                  </CButton>
-                </CCol>
-                <CCol col="6">
-                  <CButton block color="twitter">
-                    Twitter
-                  </CButton>
-                </CCol>
-              </CRow>
+              <CButton color="success" block>Create Account</CButton>
             </CCardFooter>
           </CCard>
         </CCol>
@@ -60,6 +50,26 @@
 
 <script>
 export default {
-  name: 'Register'
-}
+  name: "Register",
+  data() {
+    return {
+      form: {
+        id: "",
+        password: "",
+        email: "",
+
+        firstName: "",
+        lastName: "",
+        displayName: "",
+      },
+
+      confirmPassword: "",
+    };
+  },
+  methods: {
+    register() {
+      console.log(this.form)
+    }
+  },
+};
 </script>
